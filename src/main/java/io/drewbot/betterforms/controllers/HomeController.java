5af80487ea570a18b407096c364d1effbb740b95
Drew Botka <drewbotka@gmail.com>
2017-09-26 10:40:15 -0400

    added secret model and factored into controller and form

--- src/main/java/io/drewbot/betterforms/controllers/HomeController.java
+++ src/main/java/io/drewbot/betterforms/controllers/HomeController.java
@@ -1,13 +1,31 @@
 package io.drewbot.betterforms.controllers;
 
+import io.drewbot.betterforms.models.Secret;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
+
+    private ArrayList<Secret> mySecrets = new ArrayList<>();
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("secret", new Secret());
+        model.addAttribute("mySecrets", mySecrets);
         return "index";
     }
- }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String index(@ModelAttribute Secret secret) {
+        System.out.println(secret);
+        mySecrets.add(secret);
+        return "redirect:/";
+    }
+}
