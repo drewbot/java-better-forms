3a05eb06ecbec40252e0dc78cac4974da179893a
Drew Botka <drewbotka@gmail.com>
2017-09-27 09:43:09 -0400

    added bootstap

--- src/main/java/io/drewbot/betterforms/controllers/HomeController.java
+++ src/main/java/io/drewbot/betterforms/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package io.drewbot.betterforms.controllers;
 
 import io.drewbot.betterforms.models.Secret;
+import io.drewbot.betterforms.repositories.SecretRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -13,19 +15,20 @@ import java.util.ArrayList;
 @Controller
 public class HomeController {
 
-    private ArrayList<Secret> mySecrets = new ArrayList<>();
+    @Autowired
+    private SecretRepository secretRepo;
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("secret", new Secret());
-        model.addAttribute("mySecrets", mySecrets);
+        model.addAttribute("mySecrets", secretRepo.findAll());
         return "index";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String index(@ModelAttribute Secret secret) {
         System.out.println(secret);
-        mySecrets.add(secret);
+        secretRepo.save(secret);
         return "redirect:/";
     }
 }
